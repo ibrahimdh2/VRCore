4aaafee602e3c91d785499c83a4def54f1b13d64
Ibrahim  Nasir <ibrahim.nasirdh@gmail.com>
2025-08-05 17:23:56 +0500

    Added Colliders and Rigidbody to Cars

--- Assets/Scripts/TrafficLightsSyncher.cs
+++ Assets/Scripts/TrafficLightsSyncher.cs
@@ -1,36 +1,14 @@
-#if UNITY_EDITOR
-using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
 
-public class TrafficLightsSyncherEditor : EditorWindow
+public class TrafficLightsSyncher : MonoBehaviour
 {
-    [MenuItem("Tools/Traffic Lights/Set Children")]
-    public static void SetChildren()
+    public List<TrafficLight> forwardOrRight;
+    public List<TrafficLight> forwardOnly;
+    public List<TrafficLight> leftOrBack;
+    
+    public void ChangeSignals(LightState state)
     {
-        int i = 0;
-        Debug.Log("TrafficLightsSyncher: Method called");
-
-        var trafficLights = GameObject.FindObjectsByType<TrafficLight>(FindObjectsSortMode.InstanceID);
-
-        foreach (var trafficLight in trafficLights)
-        {
-            foreach (GameObject g in trafficLight.lightObject)
-            {
-                if (g == null) continue;
-
-                if (g.transform.parent != trafficLight.transform)
-                {
-                    Undo.SetTransformParent(g.transform, trafficLight.transform, "Set Parent for Light");
-                    g.name = "LightBlocker";
-                    EditorUtility.SetDirty(g);
-                }
-                i++;
-            }
-
-            EditorUtility.SetDirty(trafficLight);
-        }
-
-        Debug.Log($"TrafficLightsSyncher: Done. Processed {i} light objects.");
+       
     }
 }
-#endif
